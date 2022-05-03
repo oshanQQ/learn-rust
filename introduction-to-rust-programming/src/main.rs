0686fab17eb05407f809281c17fc10a8e12ccfe1
oshanQQ <oshanqq@gmail.com>
2022-05-03 23:58:40 +0900

    ✨ Implement iterator by myself

--- introduction-to-rust-programming/src/main.rs
+++ introduction-to-rust-programming/src/main.rs
@@ -1,8 +1,29 @@
 fn main() {
-    let byte_array = [b'h', b'e', b'l', b'l', b'o'];
-    print(Box::new(byte_array));
+    let it = Iter {
+        current: 0,
+        max: 10,
+    };
+    for num in it {
+        println!("{}", num);
+    }
 }
 
-fn print(s: Box<[u8]>) {
-    println!("{:?}", s);
+struct Iter {
+    current: usize,
+    max: usize,
+}
+
+// Iterator型を実装するために、2つのことを行う必要がある
+impl Iterator for Iter {
+    type Item = usize; // 出力する型の紐づけ
+
+    // next()メソッドの実装
+    fn next(&mut self) -> Option<usize> {
+        self.current += 1;
+        if self.current - 1 < self.max {
+            Some(self.current - 1)
+        } else {
+            None
+        }
+    }
 }
