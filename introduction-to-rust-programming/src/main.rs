0f9284bdeae88a0d5891e857756f9527520d39d8
oshanQQ <oshanqq@gmail.com>
2022-05-04 11:33:49 +0900

    ✨ Add examples of assertion macro

--- introduction-to-rust-programming/src/main.rs
+++ introduction-to-rust-programming/src/main.rs
@@ -1,35 +1,8 @@
-enum Emotion {
-    Anger,
-    Happy,
-}
-
-trait Emotional {
-    fn get_happy(&mut self) -> String;
-    fn get_anger(&mut self) -> String;
-    fn tell_state(&mut self) -> String;
-}
-
-struct HappyPerson {
-    name: String,
-    state: Emotion,
-}
-
-impl Emotional for HappyPerson {
-    fn get_anger(&mut self) -> String {
-        unimplemented!()
-    }
-    fn get_happy(&mut self) -> String {
-        format!("{} is always happy.", self.name)
-    }
-    fn tell_state(&mut self) -> String {
-        todo!()
-    }
-}
-
 fn main() {
-    let mut p = HappyPerson {
-        name: "ahiahi".to_string(),
-        state: Emotion::Happy,
-    };
-    println!("{}", p.get_happy());
+    assert!(true);
+    assert_eq!(1, 1);
+    assert_ne!(1, 0);
+    debug_assert!(false);
+    debug_assert_eq!(1, 1);
+    debug_assert_ne!(1, 0);
 }
