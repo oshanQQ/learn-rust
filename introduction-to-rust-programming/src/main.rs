b1b54052ec3b2f2cf20209f197dee8da1d0219c2
oshanQQ <oshanqq@gmail.com>
2022-05-04 23:38:33 +0900

    ✨ Dunamic dispatch

--- introduction-to-rust-programming/src/main.rs
+++ introduction-to-rust-programming/src/main.rs
@@ -31,4 +31,10 @@ fn main() {
     dove.tweet();
     dove.tweet_twice();
     dove.shout();
+
+    let duck = Duck {};
+    let bird_vec: Vec<Box<dyn Tweet>> = vec![Box::new(dove), Box::new(duck)];
+    for bird in bird_vec {
+        bird.tweet();
+    }
 }
