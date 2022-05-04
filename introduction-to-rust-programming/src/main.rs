66c6d0b0821813e4f22987b8fb29daf8545a7255
oshanQQ <oshanqq@gmail.com>
2022-05-04 11:53:54 +0900

    ✨ Add trait to struct with derive

--- introduction-to-rust-programming/src/main.rs
+++ introduction-to-rust-programming/src/main.rs
@@ -1,8 +1,40 @@
+#[derive(Eq, PartialEq)]
+struct A(i32);
+
+#[derive(PartialEq, PartialOrd)]
+struct B(f32);
+
+#[derive(Clone, Copy)]
+struct C;
+
+#[derive(Clone)]
+struct D;
+
+#[derive(Debug)]
+struct E;
+
+#[derive(Default)]
+struct F;
+
 fn main() {
-    assert!(true);
-    assert_eq!(1, 1);
-    assert_ne!(1, 0);
-    debug_assert!(false);
-    debug_assert_eq!(1, 1);
-    debug_assert_ne!(1, 0);
+    // Aは比較一致可能
+    println!("{:?}", A(0) == A(1));
+
+    // Bは大証比較可能
+    println!("{:?}", B(1.0) > B(0.0));
+
+    // Cはムーブではなくコピーされる
+    let c0 = C;
+    let _c1 = c0;
+    let _c2 = c0; // Cがムーブなら、c0は_c1へムーブしているはずなので、ここでコンパイルエラー
+
+    // Dはclone可能
+    let d0 = D;
+    let _d1 = d0.clone();
+
+    // Eはデバッグプリント可能
+    println!("{:?}", E);
+
+    // Fはdefault可能
+    let _f = F::default();
 }
