bd9d6a17843f562102cf59d6f4796c0b386aa19f
oshanQQ <oshanqq@gmail.com>
2022-05-05 15:00:41 +0900

    ✨ Make multiple thred

--- introduction-to-rust-programming/src/main.rs
+++ introduction-to-rust-programming/src/main.rs
@@ -1,40 +1,8 @@
-trait Tweet {
-    fn tweet(&self);
-
-    fn tweet_twice(&self) {
-        self.tweet();
-        self.tweet();
-    }
-
-    fn shout(&self) {
-        println!("Uooooooohhh!!!");
-    }
-}
-
-struct Dove;
-struct Duck;
-
-impl Tweet for Dove {
-    fn tweet(&self) {
-        println!("Coo!");
-    }
-}
-
-impl Tweet for Duck {
-    fn tweet(&self) {
-        println!("Quack!");
-    }
-}
+use std::thread;
 
 fn main() {
-    let dove = Dove {};
-    dove.tweet();
-    dove.tweet_twice();
-    dove.shout();
-
-    let duck = Duck {};
-    let bird_vec: Vec<Box<dyn Tweet>> = vec![Box::new(dove), Box::new(duck)];
-    for bird in bird_vec {
-        bird.tweet();
-    }
+    let handlle = thread::spawn(|| {
+        println!("Hello, world!");
+    });
+    dbg!(handlle.join());
 }
